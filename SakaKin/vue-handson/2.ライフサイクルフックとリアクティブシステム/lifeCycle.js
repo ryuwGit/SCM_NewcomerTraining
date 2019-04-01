1a79797255e8bb14eb95aae822ad05607aebd954
KiNShiR <g.kinshiro83@gmail.com>
2019-04-01 18:00:04 +0900

    メモ追記

--- SakaKin/vue-handson/2.ライフサイクルフックとリアクティブシステム/lifeCycle.js
+++ SakaKin/vue-handson/2.ライフサイクルフックとリアクティブシステム/lifeCycle.js
@@ -8,13 +8,23 @@ const vm = new Vue({
                 console.log(`メッセージ:${newVal},${oldVal}`);
             }
     },
+    /* メモ：公式docによると'mounted : 処理'と書かれていたからその通り書いたが動かず
+    答えを見るとこの書き方で、実装したら動いた。
+    これに関しては、謎 */
     mounted(){
+        /* メモ：vueではmountedの処理内にアロー関数はだめと書かれていたので
+        最初普通にfunction()で書いていたが動かず。調べてみると、settimeoutの内部の関数は呼び出し元が
+        グローバルオブジェクトとなるらしく(Qiita調べなので信ぴょう性？？)「どのmessageや！？」となっていたらしい
+        今回のアロー関数はmountedの中というよりsettimeout内の処理と扱われていたと考えると、
+        アロー関数ではthisを親スコープから探した結果のmessageで固定できるのでうまくいき、
+        function()だと実行時の呼び出し元であるグローバルを見るからダメでした、と納得 */
         setTimeout(() => {
                 this.message = 'こんにちは　世界'
             },2000)
     }
 })
 
+// これでも普通に動いた、watchは
 // vm.$watch('message', function(newVal, oldVal){
-//     console.log(this.message);
+//     console.log(`メッセージ:${newVal},${oldVal}`);
 // })
