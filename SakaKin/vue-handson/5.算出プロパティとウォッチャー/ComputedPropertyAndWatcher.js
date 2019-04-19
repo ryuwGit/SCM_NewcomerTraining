5f6f893b7419ab114a8434436b5e29e1aa9aa2c3
KiNShiR <g.kinshiro83@gmail.com>
2019-04-19 10:40:43 +0900

    implimenting

--- SakaKin/vue-handson/5.算出プロパティとウォッチャー/ComputedPropertyAndWatcher.js
+++ SakaKin/vue-handson/5.算出プロパティとウォッチャー/ComputedPropertyAndWatcher.js
@@ -4,9 +4,22 @@ new Vue({
       firstName: '',
       lastName: ''
     },
-    computed: {
-      fullName: function() {
-        return this.firstName + ' ' + this.lastName
+    // methods: {
+    //   fullName: function() {
+    //     return this.firstName + ' ' + this.lastName
+    //   }
+    // },
+    // computed: {
+    //   fullName: function() {
+    //     return this.firstName + ' ' + this.lastName
+    //   }
+    // },
+    watch: {
+      firstName: function(val) {
+        this.fullName = val + ' ' + this.lastName
+      },
+      lastName: function(val) {
+        this.fullName = val + ' ' + this.firstName
       }
     }
   })
